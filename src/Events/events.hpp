a082ec4018fd36bea2c26ef65f5a9f4ed428ea34
Ildar <kashaev99@icloud.com>
2020-07-03 23:14:11 +0500

    Add window folder

--- src/Events/events.hpp
+++ src/Events/events.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <GLFW/glfw3.h>
-#include "../window.hpp"
+#include "../Window/window.hpp"
 #include <iostream>
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include "window.hpp"
+#include "Window/window.hpp"
 #include "Events/events.hpp"
 
 int main(void) {
     /* Create a windowed mode window and its OpenGL context */
     Window::initialize(640, 480, "Syzygy Engine");
     Events::initialize();
-    
+
     /* Loop until the user closes the window */
     while (!Window::isShouldClose()) {
         /* Swap front and back buffers */
